72120ce95ee00d74b6da728219b15ef5bc37b63d
Diana Viglucci <dianaviglucci@Dianas-MBP.lan>
2022-01-21 16:26:01 -0500

    can view app in browswer with css

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const layout = require('./views/layout');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send(layout(''));
+});
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
